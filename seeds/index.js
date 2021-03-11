3c02b88e057260e8c81498846d8644476dfcd2c8
AKP-13 <alexpeirson@hotmail.com>
2021-03-11 10:10:57 +0000

    Update model to include random price and image, display on show page

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: "https://source.unsplash.com/collection/1114848",
+            description:
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, sint. Explicabo, veniam suscipit. Nesciunt inventore vero iusto dignissimos suscipit expedita id odit dolorum atque optio illum, dolorem, adipisci omnis alias!",
+            price,
         });
         await camp.save();
     }
