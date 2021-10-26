e81be9058fc2b07d2f0a2a3049b285368da63d00
AKP-13 <alexpeirson@hotmail.com>
2021-10-26 22:27:50 +0100

    Run prettier

--- seeds/index.js
+++ seeds/index.js
@@ -1,39 +1,39 @@
-const mongoose = require("mongoose");
-const cities = require("./cities");
-const { places, descriptors } = require("./seedHelpers");
-const Campground = require("../models/campground");
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
 });
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 30) + 10;
-        const camp = new Campground({
-            title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            image: "https://source.unsplash.com/collection/1114848",
-            description:
-                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, sint. Explicabo, veniam suscipit. Nesciunt inventore vero iusto dignissimos suscipit expedita id odit dolorum atque optio illum, dolorem, adipisci omnis alias!",
-            price,
-        });
-        await camp.save();
-    }
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 10;
+    const camp = new Campground({
+      title: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: 'https://source.unsplash.com/collection/1114848',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, sint. Explicabo, veniam suscipit. Nesciunt inventore vero iusto dignissimos suscipit expedita id odit dolorum atque optio illum, dolorem, adipisci omnis alias!',
+      price,
+    });
+    await camp.save();
+  }
 };
 
 seedDB().then(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 });
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,6 +1,6 @@
 // Pass in func, this returns a new function that has func executed and then passes any errors to next
 module.exports = (func) => {
-    return (req, res, next) => {
-        func(req, res, next).catch(next);
-    };
+  return (req, res, next) => {
+    func(req, res, next).catch(next);
+  };
 };
